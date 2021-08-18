e5745bfeddca0fa035163f584d81a63f458e99fc
Fan Mo <mofan1992@gmail.com>
2021-08-18 10:48:45 +0200

    connxr mit license

--- deep-learning-server/examples/aggregation.c
+++ deep-learning-server/examples/aggregation.c
@@ -11,6 +11,7 @@ COPYRIGHT AND LICENSING
 See the `LICENSE_MIT.markdown` file in the Veracruz deep learning server 
 example repository root directory for copyright and licensing information.
 Based on darknet, YOLO LICENSE https://github.com/pjreddie/darknet/blob/master/LICENSE
+Based on cONNXr, MIT LICENSE https://github.com/alrevuelta/cONNXr/blob/master/LICENSE
 */
 
 #include "darknet.h"
