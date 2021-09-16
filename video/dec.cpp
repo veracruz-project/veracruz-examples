7ca33732edd2321a1b26ee4f0a29843ffa50cba7
Guilhem Bryant <Guilhem.Bryant@arm.com>
2021-09-16 11:24:57 +0100

    Add licensing and authorship headers

--- video/dec.cpp
+++ video/dec.cpp
@@ -1,3 +1,16 @@
+/*
+H264 video decoding example
+
+AUTHORS
+
+The Veracruz Development Team.
+
+COPYRIGHT AND LICENSING
+
+See the `LICENSE_MIT.markdown` file in the video decoding example's root
+directory for copyright and licensing information.
+*/
+
 #include <stdlib.h>
 
 #include "h264dec.h"
