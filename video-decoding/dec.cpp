b6d44f4736bd4d9150634cd75738f11ca09582de
Guilhem Bryant <Guilhem.Bryant@arm.com>
2021-09-23 12:30:14 +0100

    Add callback called whenever a frame is ready Update openh264-dec

--- video-decoding/dec.cpp
+++ video-decoding/dec.cpp
@@ -13,9 +13,20 @@ directory for copyright and licensing information.
 
 #include <stdlib.h>
 
+#include "codec_def.h"
 #include "h264dec.h"
 
+char run = 0;
+
+// Callback called by the H.264 decoder whenever a frame is decoded and ready
+void onFrameReady(SBufferInfo *bufInfo) {
+	if (!run) {
+		printf("width:%d height:%d stride[0]:%d stride[1]:%d\n", bufInfo->UsrData.sSystemBuffer.iWidth, bufInfo->UsrData.sSystemBuffer.iHeight, bufInfo->UsrData.sSystemBuffer.iStride[0], bufInfo->UsrData.sSystemBuffer.iStride[1]);
+		run = 1;
+	}
+}
+
 int main() {
-	int32_t x = h264_decode("in.h264", "output");
+	int32_t x = h264_decode("in.h264", NULL, false, &onFrameReady);
 	return x;
 }
