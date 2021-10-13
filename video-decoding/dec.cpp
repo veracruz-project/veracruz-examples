4f0c55f7f6ae8551d6dc3308f96b1f6d713d4b5d
Guilhem Bryant <Guilhem.Bryant@arm.com>
2021-10-13 18:46:47 +0100

    Improve coding style and documentation

--- video-decoding/dec.cpp
+++ video-decoding/dec.cpp
@@ -16,17 +16,34 @@ directory for copyright and licensing information.
 #include "codec_def.h"
 #include "h264dec.h"
 
+/* Whether the callback executed already */
 char run = 0;
 
-// Callback called by the H.264 decoder whenever a frame is decoded and ready
-void onFrameReady(SBufferInfo *bufInfo) {
-	if (!run) {
-		printf("width:%d height:%d stride[0]:%d stride[1]:%d\n", bufInfo->UsrData.sSystemBuffer.iWidth, bufInfo->UsrData.sSystemBuffer.iHeight, bufInfo->UsrData.sSystemBuffer.iStride[0], bufInfo->UsrData.sSystemBuffer.iStride[1]);
-		run = 1;
-	}
+/* Callback called by the H.264 decoder whenever a frame is decoded and ready.
+ * Prints metadata about the video
+ */
+void onFrameReady(SBufferInfo *bufInfo)
+{
+    if (!run) {
+        printf("width:%d height:%d stride[0]:%d stride[1]:%d\n",
+		       bufInfo->UsrData.sSystemBuffer.iWidth,
+			   bufInfo->UsrData.sSystemBuffer.iHeight,
+			   bufInfo->UsrData.sSystemBuffer.iStride[0],
+			   bufInfo->UsrData.sSystemBuffer.iStride[1]);
+        run = 1;
+    }
 }
 
-int main() {
-	int32_t x = h264_decode("in.h264", NULL, false, &onFrameReady);
-	return x;
+/* Decode an H.264 video.
+ * Input:
+ *   - video filename
+ *   - output filename: where the frames are saved
+ *   - whether frames should be saved
+ *   - callback called whenever a frame is decoded and ready
+ * Output: error code
+ */
+int main()
+{
+    int32_t x = h264_decode("in.h264", NULL, false, &onFrameReady);
+    return x;
 }
