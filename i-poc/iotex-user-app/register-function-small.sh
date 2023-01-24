9ff8b868850f5fb980ae75766af7cbc940cf1975
Alexandre Peixoto Ferreira <alexandre.ferreira@arm.com>
2023-01-24 13:46:03 -0600

    Fixes fro veracruz-nitro (1.3.3) and miscelaneous changes
    Signed-off-by: Alexandre Peixoto Ferreira <alexandre.ferreira@arm.com>

--- i-poc/iotex-user-app/register-function-small.sh
+++ i-poc/iotex-user-app/register-function-small.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-HOSTIP=127.0.0.1
-HOSTPORT=5010
+CCFAAS_HOST=${1:-"127.0.0.1"}
+CCFAAS_PORT=${2:-"5010"}
+
+echo "Acessing CCFaaS at ${CCFAAS_HOST}:${CCFAAS_PORT}"
+
 JSON_FUNCTION_POLICY=$(cat function_policy_small.json)
 FUNCTION_NAME="vod_small"
 
@@ -9,33 +12,22 @@ BUNDLE_PATH="."
 client_path="../../veracruz-client"
 PROGRAM_PATH="."
 
-function pretty_print_wget() {
-	echo "$1" | jq 2>/dev/null
-	if [ $? -gt 0 ]
-	then
-		echo "$1"
-	else
-		if [ ! -z "$2" ]
-		then
-			echo "$1" | jq 2>/dev/null > $2
-		fi
-	fi
-	if [ ! -z "$3" ]
-	then
-		echo "$1" >  $3
-	fi
-}
+. register-function-aux.sh
 
 echo "=============Deleting function"
-pretty_print_wget "$(curl -X DELETE http://${HOSTIP}:${HOSTPORT}/function/${FUNCTION_NAME} 2>&1)"
+pretty_print_wget "$(curl -X DELETE http://${CCFAAS_HOST}:${CCFAAS_PORT}/function/${FUNCTION_NAME} 2>&1)"
 
 echo "=============Registering function"
-pretty_print_wget "$(wget -q --content-on-error -nv  -O- --post-data="${JSON_FUNCTION_POLICY}" --header='Content-Type:application/json' http://${HOSTIP}:${HOSTPORT}/function 2>&1)"
+pretty_print_wget "$(wget -q --content-on-error -nv  -O- --post-data="${JSON_FUNCTION_POLICY}" --header='Content-Type:application/json' http://${CCFAAS_HOST}:${CCFAAS_PORT}/function 2>&1)"
 
 echo "=============Provisioning program"
-pretty_print_wget "$(wget -q --content-on-error -nv  -O- --post-file=$PROGRAM_PATH/detector.wasm --header='Content-Type:application/json' http://${HOSTIP}:${HOSTPORT}/function/${FUNCTION_NAME}/program/$(echo -n "/program/detector.wasm" | base64) 2>&1)"
+load_file_veracruz "${FUNCTION_NAME}" "program" "/program/detector.wasm" "$PROGRAM_PATH/detector.wasm" 
+#pretty_print_wget "$(wget -q --content-on-error -nv  -O- --post-file=$PROGRAM_PATH/detector.wasm --header='Content-Type:application/json' http://${CCFAAS_HOST}:${CCFAAS_PORT}/function/${FUNCTION_NAME}/program/$(echo -n "/program/detector.wasm" | base64) 2>&1)"
 
 echo "=============Provisioning data"
-pretty_print_wget "$(wget -q --content-on-error -nv  -O- --post-file=$BUNDLE_PATH/input/coco.names --header='Content-Type:application/json' http://${HOSTIP}:${HOSTPORT}/function/${FUNCTION_NAME}/data_file/$(echo -n "/program_data/coco.names" | base64) 2>&1)"
-pretty_print_wget "$(wget -q --content-on-error -nv  -O- --post-file=$BUNDLE_PATH/input/yolov3-tiny.cfg --header='Content-Type:application/json' http://${HOSTIP}:${HOSTPORT}/function/${FUNCTION_NAME}/data_file/$(echo -n "/program_data/yolov3.cfg" | base64) 2>&1)"
-pretty_print_wget "$(wget -q --content-on-error -nv  -O- --post-file=$BUNDLE_PATH/input/yolov3-tiny.weights --header='Content-Type:application/json' http://${HOSTIP}:${HOSTPORT}/function/${FUNCTION_NAME}/data_file/$(echo -n "/program_data/yolov3.weights" | base64) 2>&1)"
+load_file_veracruz "${FUNCTION_NAME}" "data_file" "/program_data/coco.names" "$BUNDLE_PATH/input/coco.names"
+load_file_veracruz "${FUNCTION_NAME}" "data_file" "/program_data/yolov3.cfg" "$BUNDLE_PATH/input/yolov3-tiny.cfg"
+load_file_veracruz "${FUNCTION_NAME}" "data_file" "/program_data/yolov3.weights" "$BUNDLE_PATH/input/yolov3-tiny.weights"
+#pretty_print_wget "$(wget -q --content-on-error -nv  -O- --post-file=$BUNDLE_PATH/input/coco.names --header='Content-Type:application/json' http://${CCFAAS_HOST}:${CCFAAS_PORT}/function/${FUNCTION_NAME}/data_file/$(echo -n "/program_data/coco.names" | base64) 2>&1)"
+#pretty_print_wget "$(wget -q --content-on-error -nv  -O- --post-file=$BUNDLE_PATH/input/yolov3-tiny.cfg --header='Content-Type:application/json' http://${CCFAAS_HOST}:${CCFAAS_PORT}/function/${FUNCTION_NAME}/data_file/$(echo -n "/program_data/yolov3.cfg" | base64) 2>&1)"
+#pretty_print_wget "$(wget -q --content-on-error -nv  -O- --post-file=$BUNDLE_PATH/input/yolov3-tiny.weights --header='Content-Type:application/json' http://${CCFAAS_HOST}:${CCFAAS_PORT}/function/${FUNCTION_NAME}/data_file/$(echo -n "/program_data/yolov3.weights" | base64) 2>&1)"
